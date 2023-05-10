4682f7e89172dfb8c5b1a1edc9893436bb8f460f
Ognjen Jevremovic <ognjen.jevremovic@everseen.com>
2023-05-10 19:09:55 +0200

    chore: [nx migration] update-babel-preset

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from "@storybook/core-common";
 export const rootMain: StorybookConfig = {
-  addons: []
+  addons: [],
+  stories: [],
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
